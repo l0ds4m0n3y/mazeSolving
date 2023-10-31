063650950b94d9eb1757e8fba41edbcfe4b202df
l0ds4m0n3y <dluna777@outlook.com>
2023-10-31 11:10:47 -0700

    initializing

---
+++ QueueMazeSolver.java
@@ -0,0 +1,12 @@
+public class QueueMazeSolver implements MazeSolver{
+    private MazeGUI gui;
+
+    QueueMazeSolver(){
+        gui = new MazeGUI(this);
+    }
+    
+    @Override
+    public void solve(char[][] maze, int startR, int startC, int endR, int endC) {
+        throw new UnsupportedOperationException("Unimplemented method 'solve'");
+    }
+}
---
+++ QueueMazeSolverMain.java
@@ -0,0 +1,5 @@
+public class QueueMazeSolverMain {
+    public static void main(String[] args) {
+        new QueueMazeSolver();
+    }
+}
