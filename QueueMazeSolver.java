672d8f3e0ad23b73ffe1f95d0f5bf78edaf6e01e
l0ds4m0n3y <103957548+l0ds4m0n3y@users.noreply.github.com>
2023-10-31 15:05:44 -0700

    Update QueueMazeSolver.java

--- QueueMazeSolver.java
+++ QueueMazeSolver.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class QueueMazeSolver implements MazeSolver{
     private MazeGUI gui;
 
@@ -7,6 +11,27 @@ public class QueueMazeSolver implements MazeSolver{
     
     @Override
     public void solve(char[][] maze, int startR, int startC, int endR, int endC) {
-        throw new UnsupportedOperationException("Unimplemented method 'solve'");
+        Queue<Cell> mazeQueue = new LinkedList<>();
+        for(Cell c : checkForNeighbours(maze, startR, startC)){
+            mazeQueue.offer(c);
+        }
+        while(!mazeQueue.isEmpty()){
+            System.out.println(mazeQueue.poll().character);
+        }
+        
+    }
+    
+    public ArrayList<Cell> checkForNeighbours(char[][] maze, int row, int col){
+        ArrayList<Cell> neighbours = new ArrayList<>();
+        if(row > 0 && maze[row - 1][col] == ' ') neighbours.add(new Cell(row - 1, col, '@'));
+        if(col > 0 && maze[row][col - 1] == ' ') neighbours.add(new Cell(row, col - 1, '@'));
+        if(col < maze[0].length && maze[row][col + 1] == ' ') neighbours.add(new Cell(row, col + 1, '@'));
+        if(row < maze.length && maze[row + 1][col] == ' ') neighbours.add(new Cell(row + 1, col, '@'));
+
+        return neighbours;
+    }
+
+    public boolean checkIfEnd(Cell cell){
+        return cell.getChar() == '*';
     }
 }
